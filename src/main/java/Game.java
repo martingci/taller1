f463be09497b554d6af3a743f4ada56eedb3f418
martingci <m.carrasco29@ufromail.cl>
2024-08-21 09:40:42 -0400

    Se crean métodos para inicializar matriz con cartas.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-        jugar();
+        jugarMenu();
     }
     public static int userOption(String text) {
         Scanner scanner = new Scanner(System.in);
@@ -24,13 +24,13 @@ public class Game {
     public static void optionElection(int option) {
         switch (option) {
             case 1:
-                System.out.println("Has seleccionado la opción 1");
+                inicioJuego();
                 break;
             default:
                 System.out.println("Cerrando programa");
         }
     }
-    public static void jugar() {
+    public static void jugarMenu() {
         int opcion;
         do {
             elections();
@@ -41,5 +41,42 @@ public class Game {
     public static void elections() {
         System.out.println("1. Jugar");
         System.out.println("0. Salir");
+    }
+    public static String [][] crearMatrizCartas () {
+        return (new String [12][2]);
+    }
+    public static void inicioJuego() {
+        String [][] cards = crearMatrizCartas();
+        cards = inicializarCartasJuego(cards);
+        for (int i = 0; i < cards.length; i++) {
+            System.out.println(cards[i][0]);
+            System.out.println(cards[i][1]);
+        }
+
+    }
+    public static String [][] AgregarCartas (String [][] matriz, String nombre, String valor) {
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][0] == null) {
+                matriz[i][0] = nombre;
+                matriz[i][1] = valor;
+                break;
+            }
+        }
+        return matriz;
+    }
+    public static String [][] inicializarCartasJuego(String [][] matriz) {
+        matriz = AgregarCartas(matriz, "As", "11");
+        matriz = AgregarCartas(matriz, "2", "2");
+        matriz = AgregarCartas(matriz, "3", "3");
+        matriz = AgregarCartas(matriz, "4", "4");
+        matriz = AgregarCartas(matriz, "5", "5");
+        matriz = AgregarCartas(matriz, "6", "6");
+        matriz = AgregarCartas(matriz, "7", "7");
+        matriz = AgregarCartas(matriz, "8", "8");
+        matriz = AgregarCartas(matriz, "9", "9");
+        matriz = AgregarCartas(matriz, "Jota", "10");
+        matriz = AgregarCartas(matriz, "Reina", "10");
+        matriz = AgregarCartas(matriz, "Rey", "10");
+        return matriz;
     }
 }
